8d20df5188d5cd900a741968f44d6b88800b4233
bnichs5 <bnichs51259@gmail.com>
2021-05-16 19:31:59 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -3,7 +3,7 @@
 // note that this is loaded from wp-config.d by the real wp-config.php
 
     !defined('DB_USER')          && define('DB_USER',          getenv('QOVERY_DATABASE_WORDPRESS_USERNAME')        ?: 'superuser');
-    !defined('DB_NAME')          && define('DB_NAME',          getenv('QOVERY_DATABASE_WORDPRESS_NAME')        ?: 'qwerty');
+    !defined('DB_NAME')          && define('DB_NAME',          getenv('QOVERY_DATABASE_WORDPRESS_NAME')        ?: 'mysql-qwerty');
     !defined('DB_PASSWORD')      && define('DB_PASSWORD',      getenv('QOVERY_DATABASE_WORDPRESS_PASSWORD')    ?: 'GseH6y0YL9URNc5J');
     !defined('DB_HOST')          && define('DB_HOST',          getenv('QOVERY_DATABASE_WORDPRESS_HOST')        ?: 'qwerty-r63ajcoriwo4nt8m-svc.qovery.io');
 
